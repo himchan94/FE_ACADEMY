2fbbfa0b274dd4b9c54bd056464e2841d1bc098e
himchan94 <himchan9494@gmail.com>
2022-01-24 17:25:10 +0900

    feat: Completed vanillaJS hackerNews

---
+++ app-debug.js
@@ -0,0 +1,72 @@
+const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+const ajax = new XMLHttpRequest();
+const content = document.createElement("div");
+const container = document.getElementById("root");
+const ul = document.createElement("ul");
+
+const store = {
+  currentPage: 1,
+};
+
+const getData = (url) => {
+  ajax.open("GET", url, false);
+  ajax.send();
+
+  return JSON.parse(ajax.response);
+};
+
+const newsFeed = () => {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
+
+  newsList.push("<ul>");
+  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
+    newsList.push(`
+    <li>
+      <a href="#/show/${newsFeed[i].id}">
+      ${newsFeed[i].title} (${newsFeed[i].comments_count})
+      </a>
+    </li>
+  `);
+  }
+
+  newsList.push("</ul>");
+  newsList.push(`
+    <div>
+      <a href="#/page/${store.currentPage - 1}">이전 페이지</a> 
+      <a href="#/page/${store.currentPage + 1}">다음 페이지</a> 
+    </div>
+  `);
+
+  container.innerHTML = newsList.join("");
+};
+
+const newsDetail = () => {
+  const id = location.hash.substring(7);
+  const newsContent = getData(CONTENT_URL.replace("@id", id));
+  container.innerHTML = `
+  <h1>${newsContent.title}</h1>
+
+  <div>
+    <a href="#">목록으로</a>
+  </div>
+  `;
+};
+
+const router = () => {
+  const routePath = location.hash;
+
+  if (routePath === "") {
+    newsFeed();
+  } else if (routePath.indexOf("#/page/" >= 0)) {
+    store.currentPage = Number(routePath.substring(7));
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+};
+
+window.addEventListener("hashchange", router);
+
+router();
