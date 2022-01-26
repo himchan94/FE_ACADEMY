33c594f30c36b7e3e79d96218ecfee8717dfdad6
himchan94 <himchan9494@gmail.com>
2022-01-26 14:06:14 +0900

    feat: Applying Interface

--- app.ts
+++ app.ts
@@ -1,33 +1,33 @@
 import { NumberLiteralType } from "typescript";
 
-type Store = {
+interface Store {
   currentPage: number;
   feeds: NewsFeed[];
-};
+}
 
-type News = {
+interface News {
   id: number;
   time_ago: string;
   title: string;
   url: string;
   user: string;
   content: string;
-};
+}
 
-type NewsFeed = News & {
+interface NewsFeed extends News {
   comments_count: number;
   points: NumberLiteralType;
   read?: boolean;
-};
+}
 
-type NewsDetail = News & {
+interface NewsDetail extends News {
   comments: NewsComment[];
-};
+}
 
-type NewsComment = News & {
+interface NewsComment extends News {
   comments: NewsComment[];
   level: number;
-};
+}
 
 const container: HTMLElement | null = document.getElementById("root");
 const ajax: XMLHttpRequest = new XMLHttpRequest();
